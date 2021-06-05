14635c021dd8419fac57e0b1b0d1c41da3c32d31
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-05 20:08:19 +0200

    Adding component and importing reactiveModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CardFromComponent } from './card-from/card-from.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFromComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-from/card-from.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-from',
+  templateUrl: './card-from.component.html',
+  styleUrls: ['./card-from.component.css']
+})
+export class CardFromComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
