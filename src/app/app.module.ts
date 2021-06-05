3ad880874612d62945da2e0ad0b126aa1ecd4232
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-05 20:28:48 +0200

    adding form reactive example.. look import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CardFromComponent } from './card-from/card-from.component';
 import { CardFormComponent } from './card-form/card-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardFromComponent,
     CardFormComponent
   ],
   imports: [
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//for reactiveForms
+import { FormGroup, FormControl } from '@angular/forms';
+//FormGroup fa riferimento a tutti i campi del form che devono essere validati o submittati.
+//FormControl gestisce il singolo field.
 
 @Component({
   selector: 'app-card-form',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-form.component.css']
 })
 export class CardFormComponent implements OnInit {
+
+  cardForm = new FormGroup({
+    name: new FormControl(/*Default value*/ '')
+  });
 
   constructor() { }
 
