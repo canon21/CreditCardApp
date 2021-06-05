ad77ca16ed1ae68e8742d6bdaa7807341d68de28
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-06-05 20:36:26 +0200

    Adding a little Validation on field name

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //for reactiveForms
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 //FormGroup fa riferimento a tutti i campi del form che devono essere validati o submittati.
 //FormControl gestisce il singolo field.
 
@@ -11,8 +11,10 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CardFormComponent implements OnInit {
 
-  cardForm = new FormGroup({
-    name: new FormControl(/*Default value*/ '')
+  cardForm = new FormGroup( 
+    { 
+      name: new FormControl(/*Default value*/ '', [Validators.required, Validators.minLength(3)])
+  
   });
 
   constructor() { }
